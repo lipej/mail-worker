d30eab4c398707b99a38ed67131f4c110421cdac
Felipe José <lipejose@icloud.com>
2022-01-14 14:56:30 -0300

    init spring boot project

---
+++ src/main/java/dev/lipejose/mailworker/MailworkerApplication.java
@@ -0,0 +1,13 @@
+package dev.lipejose.mailworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailworkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lipejose/mailworker/MailworkerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lipejose.mailworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailworkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
